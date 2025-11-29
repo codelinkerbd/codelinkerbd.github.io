569cff8c578e20d969d931d254a2b2feebd26ff4
Mahmud Abdur Rahman <clanlord.ikot@gmail.com>
2025-11-29 23:16:03 +0600

    deployment branch changed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,6 +24,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'codelinkerbd', // Usually your GitHub org/user name.
   projectName: 'codelinkerbd.github.io', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
